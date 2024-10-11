fd608fe72c119aeb9e7fb7cc6b9317574f04eabd
Aradhanadevi <aradhanajadeja81@gmail.com>
2024-10-11 18:56:15 +0530

    navbar final

--- src/comp/Navbar.js
+++ src/comp/Navbar.js
@@ -1,26 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import logo from './Bgimg/logo.png';
 import '../comp/Css/Navbar.css';
 
-export const CustomNavbar = () => {
+const CustomNavbar = () => {
   const [isNavbarCollapsed, setIsNavbarCollapsed] = useState(true);
   const username = localStorage.getItem('name');
 
-  // Function to toggle the navbar's collapsed state
   const handleNavbarToggle = () => {
-    setIsNavbarCollapsed((prevState) => !prevState);
+    setIsNavbarCollapsed(!isNavbarCollapsed);
   };
 
-  // Function to close navbar when clicking outside
   const handleClickOutside = (event) => {
-    if (!event.target.closest('.navbar')) {
+    if (!event.target.closest('.topnav')) {
       setIsNavbarCollapsed(true);
     }
   };
 
-  // Initialize Google Translate widget
   const initializeGoogleTranslate = () => {
     if (window.google && window.google.translate) {
       new window.google.translate.TranslateElement(
@@ -42,33 +38,27 @@ export const CustomNavbar = () => {
   }, []);
 
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="/agriportal">
-          <img src={logo} width="40" height="40" className="d-inline-block align-top" alt="Logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle onClick={handleNavbarToggle} aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav" className={`${isNavbarCollapsed ? '' : 'show'}`}>
-          <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/" onClick={handleNavbarToggle}>Login</Nav.Link>
-            <Nav.Link as={NavLink} to="/agriportal" onClick={handleNavbarToggle}>Home</Nav.Link>
-            <Nav.Link as={NavLink} to="/know" onClick={handleNavbarToggle}>Knowledge</Nav.Link>
-            <Nav.Link as={NavLink} to="/cropadvisory" onClick={handleNavbarToggle}>Advisory</Nav.Link>
-            <Nav.Link as={NavLink} to="/scehemes" onClick={handleNavbarToggle}>Schemes</Nav.Link>
-            <Nav.Link as={NavLink} to="/marketinfo" onClick={handleNavbarToggle}>Market Info</Nav.Link>
-            <Nav.Link as={NavLink} to="/weather" onClick={handleNavbarToggle}>Weather</Nav.Link>
-            {username && username !== 'undefined' && (
-              <Nav.Link as={NavLink} to="/profile" onClick={handleNavbarToggle}>Welcome, {username}</Nav.Link>
-            )}
-            <NavDropdown title="More" id="collapsible-nav-dropdown">
-              <NavDropdown.Item as={NavLink} to="/contacts" onClick={handleNavbarToggle}>Contacts</NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/about" onClick={handleNavbarToggle}>About Us</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <div id="google_translate_element" className="google-translate-container"></div>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className={`topnav ${isNavbarCollapsed ? '' : 'responsive'}`}>
+      <NavLink to="/agriportal" className="logo">
+        <img src={logo} alt="Logo" />
+      </NavLink>
+      <NavLink to="/" className="nav-link" onClick={handleNavbarToggle}>Login</NavLink>
+      <NavLink to="/agriportal" className="nav-link" onClick={handleNavbarToggle}>Home</NavLink>
+      <NavLink to="/know" className="nav-link" onClick={handleNavbarToggle}>Knowledge</NavLink>
+      <NavLink to="/cropadvisory" className="nav-link" onClick={handleNavbarToggle}>Advisory</NavLink>
+      <NavLink to="/schemes" className="nav-link" onClick={handleNavbarToggle}>Schemes</NavLink>
+      <NavLink to="/marketinfo" className="nav-link" onClick={handleNavbarToggle}>Market Info</NavLink>
+      <NavLink to="/weather" className="nav-link" onClick={handleNavbarToggle}>Weather</NavLink>
+      {username && username !== 'undefined' && (
+        <NavLink to="/profile" className="nav-link" onClick={handleNavbarToggle}>
+          Welcome, {username}
+        </NavLink>
+      )}
+      <div className="icon" onClick={handleNavbarToggle}>
+        &#9776;
+      </div>
+      <div id="google_translate_element" className="google-translate-container"></div>
+    </div>
   );
 };
 
